2ee52737e4438dd79a8bcf6fe0ab097d9a57a2c0
Shadman Kudchikar <shadmanssk@yahoo.in>
2018-08-20 14:48:04 +0530

    fix imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ClientsComponent } from './clients/clients.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ClientComponent } from './clients/client/client.component';
 import { StocksComponent } from './stocks/stocks.component';
+import { ClientComponent } from './client/client.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
